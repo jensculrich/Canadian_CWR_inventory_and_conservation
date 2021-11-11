3ed89afce048cbfd94c8a31f864cc5c0def24b42
jensculrich <jensj27@gmail.com>
2021-11-11 13:32:42 -0800

    updated data cleaning file

--- Identify_gardens_CWR_accessions.R
+++ Identify_gardens_CWR_accessions.R
@@ -1,72 +1,25 @@
-library(tidyverse)
+##GOAL: Compare Botanical Garden accession list with our Master Canadian CWR list
+##END-RESULT: A list of all CWR accessions from Botanical Garden
 
-########################################################
-# DATA TIDYING FILE FOR MULTIPLE DATA MANAGEMENT TASKS #
-########################################################
-# setwd() <- Navigate to a subfolder if needed
-
-##################################################
-# COMBINE TAXON LISTS FOR THE INVENTORY BACKBONE #
-##################################################
-
-# To combine Davidson and GRIN taxon lists
-# needed to clean the Davidson data
-# wanted one column with genus species names rather than genus and species in separate columns
-df <- read.csv("Davidson_Canada_raw_data.csv")
-
-df2 <- df %>% 
-  unite("TAXON", Wild_Relative_Genus:Wild_Relative_species, 
-        sep = " ", remove = FALSE) %>%
-  select(-Wild_Relative_Genus, -Wild_Relative_species) %>%
-  rename(PRIMARY_ASSOCIATED_CROP_COMMON_NAME = Crop)
-# write.csv(df2, "Davidson_Canada_cleaned_data.csv")
-
-###########################################################
-# SEPERATE TAXON NAMES INTO GENUS, SPECIES, INFRASPECIFIC #
-###########################################################
-
-# To break names of taxa in inventory so that each species has 
-# one column with just the species name "SPECIES",
-# second column with "RANK" (subsp. or var.),
-# third column with "INFRASPECIFIC" (subspecies or varietal name)
-# want to keep the full name "TAXON" column
-df <- read.csv("inventory.csv")
-
-df2 <- df %>% 
-  mutate(GENUS = word(TAXON, 1)) %>%
-  mutate(TAXON1 = TAXON) %>%
-  separate(TAXON1, c("SPECIES1", "SPECIES2", 
-                    "RANK", "INFRASPECIFC"), 
-           sep = "\\ ") %>%
-  unite("SPECIES", "SPECIES1", "SPECIES2", sep = " ")
-
-# write.csv(df2, "inventory.csv")  
-
-############################
-# FILTER GARDEN ACCESSIONS #
-############################
-##GOAL: Filter Garden/repository accessions for CWR and WUS taxa
-##END-RESULT: A list of all CWR accessions from Garden/repository
+setwd("~/Desktop/Grad courses/RES500/CWR project/CWR")
+master_list <- read.csv("CWR_Master_list.csv")
 library(stringr)
-
-# setwd()
-master_list <- read.csv("inventory.csv")
-
 #read data for whichever garden you want to compare
 rbg<- read.csv("UBC_bg.csv")
 
 #Most gardens have variant info in their species name so we need to seperate that out in order to compare with our Master List
 
 #rbg_replace <- gsub("\\[", "(", rbg$NAME)  #messy work-around: 
-#replace '[' with '(' because str_split_fixed function can't deal with '['
+                                         #replace '[' with '(' because str_split_fixed function can't deal with '['
 #rebinded <- cbind(rbg_replace, rbg) #rebind them together so it's recursive
+
 #is.recursive(rebinded) #check to see if the data is recursive, str_split_fixed won't work with atomic only recursive
 
 
 #this line will likely have to be slightly modified for each garden after looking at it a bit..
 split_var <- str_split_fixed(rbg$TaxonName, " var. | cv.| subsp.|[']|[(]|sp.", 2)  #seperate names into 2 columns
-# based off of different ways 'variant' 
-#categories were written in the in the database
+                                                                                   # based off of different ways 'variant' 
+                                                                                  #categories were written in the in the database
 
 total<-cbind(rbg, split_var) #put split columns back into original RBG database with proper headings
 colnames(total)[43] <- "species" 
@@ -74,16 +27,15 @@ colnames(total)[44] <- "variant"
 
 total$species <- trimws(total$species, which = c("right"))  #remove trailing white space on species names
 
+
 CWR_of_RBG<-merge(master_list,total, by.x = "sci_name", by.y = "species") #Finally, cross-reference RBG list with Master list
-#getting a list of all CWR plants in RBG
+                                                                                    #getting a list of all CWR plants in RBG
 
-# write.csv(CWR_of_RBG, "CWR_of_UBC.csv")  
+write.csv(CWR_of_RBG, "CWR_of_UBC.csv")  
 
-#####################################
-# TIDY PGRC and USDA ACCESSION DATA #
-#####################################
 ### NOV 1 - Tidying and seperating column names of PGRC accession data PGRC DATASETS ####
 pgrc_US<- read.csv("PGRC_accessions_USA.csv")
+
 
 #this line will likely have to be slightly modified for each garden after looking at it a bit..
 split_var <- str_split_fixed(pgrc_US$Taxonomy, "\\s+", 3)  #seperate names into 3 columns, \\s+ is any sized white space
@@ -122,7 +74,7 @@ total <- subset(total, select=-c(Variant_interm, Subspecies_interm, garbage, gar
 
 #giving correct name in Rank Column using iselse statement 
 total$Rank <- ifelse(total$Variant!="", "Var.",
-                     ifelse(total$Subspecies!="", "Subsp.", ""))
+                              ifelse(total$Subspecies!="", "Subsp.", ""))
 total$Infraspecific <- paste(total$Variant, total$Subspecies)
 total$Infraspecific <- trimws(total$Infraspecific, which = c("both"))  #remove trailing white space on species names
 
@@ -132,7 +84,7 @@ total$Taxon <-  paste(total$Species, total$Rank, total$Infraspecific)
 total <- subset(total, select=-c(Variant, Subspecies, Species_only))
 total <- total[, c(1,2,3,4,5,10,6,9,7,8)] #reorder into the order we want
 
-# write.csv(total, "Cleaned_PGRC_US.csv")  
+#write.csv(total, "Cleaned_PGRC_US.csv")  
 
 
 ### NOV 1 - Tidying and seperating column names of PGRC accession data USDA DATASETS####
@@ -183,5 +135,4 @@ total$Taxon <-  paste(total$Species, total$Rank, total$Infraspecific)
 total <- subset(total, select=-c(Variant, Subspecies, Species_only))
 total <- total[, c(1,2,3,4,5,6,7,12,8,11,9,10)] #reorder into the order we want
 
-# write.csv(total, "Cleaned_USDA_CAD.csv")  
-
+write.csv(total, "Cleaned_USDA_CAD.csv")  
